26fb11c5f0bcfb11518a6cae7da33fba5fdd30c0
pipu-kun <SideActivity@proton.me>
2026-03-10 12:35:50 +0000

    Add If else function

--- BMI.cpp
+++ BMI.cpp
@@ -10,12 +10,23 @@ float BMI(float bb, float tb) {
 
 
 int main() {
-    cout << "-- Selamat Datang Mahasiswa Kelas C 2025 --" << endl;
+    cout << endl;
+    cout << "--- Selamat Datang Mahasiswa Kelas C 2025 ---" << endl;
     cout << "Masukan Berat Badan (kg) : ";
     cin >> bb;
     cout << "Masukan Tinggi Badan (m) : ";
     cin >> tb;
 
-    cout << "-- HASIL --" << endl;
+    cout << endl;
+    cout << "--- HASIL ---" << endl;
     cout << "BMI anda : " << BMI(bb, tb) << endl;
+        if (BMI(bb, tb) < 18.5) {
+            cout << "Status : Berat Badan Kurang" << endl;
+        } else if (BMI(bb, tb) < 25) {
+            cout << "Status : Berat Badan Normal" << endl;
+        } else if (BMI(bb, tb) < 30) {
+            cout << "Status : Berat Badan Berlebih" << endl;
+        } else {
+            cout << "Status : Obesitas" << endl;
+        }
 }
