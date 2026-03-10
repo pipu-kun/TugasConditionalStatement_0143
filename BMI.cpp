24b729fffc2cc800a86fa1835c6aba9b58a2830c
pipu-kun <SideActivity@proton.me>
2026-03-10 12:13:54 +0000

    Add Input Function

--- BMI.cpp
+++ BMI.cpp
@@ -7,4 +7,11 @@ float BMI(float bb, float tb) {
     return bb / (tb * tb);
 }
 
-int main() {}
+int main() {
+    cout << "-- Selamat Datang Mahasiswa Kelas C 2025 --" << endl;
+    cout << "Masukan Berat Badan (kg) : ";
+    cin >> bb;
+    cout << "Masukan Tinggi Badan (m) : ";
+    cin >> tb;
+
+}
